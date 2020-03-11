d9c95f34c331ea88b93be9eca06948637274c616
sqwezzy <maks.subbotko46@gmail.com>
2020-03-11 14:11:26 +0300

    add component catalog without information

---
+++ src/app/catalog-without-information/catalog-without-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ms-catalog-without-information',
+  templateUrl: './catalog-without-information.component.html',
+  styleUrls: ['./catalog-without-information.component.scss']
+})
+export class CatalogWithoutInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
