8fa62dc56b6665dc201663eaed694dd6d4b0bd35
sqwezzy <maks.subbotko46@gmail.com>
2020-04-11 15:57:38 +0300

    fix bugs

--- src/app/admin/admin-categories/admin-categories.component.ts
+++ src/app/admin/admin-categories/admin-categories.component.ts
@@ -1,18 +1,18 @@
-import {Component, OnInit} from '@angular/core';
-import {CategoryService} from '../services/category.service';
-import {Category} from '../models/category';
-import {AdminService} from '../services/admin.service';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {CategoryService} from '../../services/category.service';
+import {Category} from '../../models/category';
+import {AdminService} from '../../services/admin.service';
 import {delay} from 'rxjs/operators';
 import {MatDialog} from '@angular/material';
-import {AddCategoryModalComponent} from '../modal-windows/add-category-modal/add-category-modal.component';
-import {SnackBarService} from '../services/snackBar.service';
+import {AddCategoryModalComponent} from '../../modal-windows/add-category-modal/add-category-modal.component';
+import {SnackBarService} from '../../services/snackBar.service';
 
 @Component({
   selector: 'ms-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  templateUrl: './admin-categories.component.html',
+  styleUrls: ['./admin-categories.component.scss']
 })
-export class AdminComponent implements OnInit {
+export class AdminCategoriesComponent implements OnInit {
   categories: Category[];
   displayedColumns: string[] = ['id', 'name', 'deleted', 'update'];
 
@@ -32,13 +32,16 @@ export class AdminComponent implements OnInit {
     this.adminService.deleteCategory(categoryId).subscribe((response: any) => {
       this.categoryService.deleteCategoryFromStorage(response.category);
       this.snackBar.showSnackBar(response.message);
-      this.categoryService.getCategoryList().subscribe(categories => {
-        this.categories = categories;
-      });
-  });
+      this.adminService.deleteManyDish(response.category).subscribe(message => console.log(message));
+    }, (error) => {
+      console.log(error);
+    });
   }
 
-  openModalAddCategory() {
+  openModalAddCategory(category: Category) {
     this.modal.open(AddCategoryModalComponent);
+    // const modalRef = this.modal.open(AddCategoryModalComponent, {
+    //   data: category,
+    // });
   }
 }
--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -1,20 +1,78 @@
 import {Injectable} from '@angular/core';
 import {Category} from '../models/category';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {CategoryService} from './category.service';
+import {Observable} from 'rxjs';
+import {ResponseCategory} from '../models/responseCategory';
+import {SERVER_URL} from '../../environments/constant';
 
 @Injectable({
   providedIn: 'root',
 })
 
 export class AdminService {
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+              private categoryService: CategoryService) {
   }
 
-  removeCategory(categoryId: string) {
-    return this.http.delete<Category[]>('http://localhost:9000/categories/' + categoryId);
+  deleteCategory(categoryId: string): Observable<string> {
+    return this.http.delete<string>(`${SERVER_URL}categories/` + categoryId);
   }
 
-  addNewCategory(category: Category) {
-    return this.http.post<Category>('http://localhost:9000/categories', category);
+  addNewCategory(category: Category): Observable<ResponseCategory> {
+    return this.http.post<ResponseCategory>(`${SERVER_URL}categories`, category);
+  }
+
+  deleteManyDish(currentCategory: Category): Observable<string> {
+    return this.http.delete<string>(`${SERVER_URL}dishes/delete/${currentCategory.code}`);
+  }
+
+  deleteDish(dishId: string): Observable<string> {
+    return this.http.delete<string>(`${SERVER_URL}dishes/${dishId}`);
+  }
+
+  addNewDish(name: string,
+             categoryCode: string,
+             composition: string,
+             description: string,
+             price: string,
+             weight: string,
+             rating: string,
+             image?: File) {
+    const fd = new FormData();
+    if (image) {
+      fd.append('image', image, image.name);
+    }
+    fd.append('name', name);
+    fd.append('categoryCode', categoryCode);
+    fd.append('rating', rating);
+    fd.append('weight', weight);
+    fd.append('price', price);
+    fd.append('composition', composition);
+    fd.append('description', description);
+    return this.http.post(`${SERVER_URL}dishes`, fd);
+  }
+
+  updateDish(dishId: string,
+             name: string,
+             categoryCode: string,
+             composition: string,
+             description: string,
+             price: string,
+             weight: string,
+             rating: string,
+             image?: File) {
+    const fd = new FormData();
+    if (image) {
+      fd.append('image', image, image.name);
+    }
+    fd.append('name', name);
+    fd.append('categoryCode', categoryCode);
+    fd.append('rating', rating);
+    fd.append('weight', weight);
+    fd.append('price', price);
+    fd.append('composition', composition);
+    fd.append('description', description);
+    return this.http.put(`${SERVER_URL}dishes/${dishId}`, fd);
   }
 }
