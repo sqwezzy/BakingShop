9c30148ba3ef0e1e6581bfe6a685e93533dc0ab0
sqwezzy <maks.subbotko46@gmail.com>
2020-04-18 19:54:35 +0300

    add simple test for category service

---
+++ src/app/services/category.service.spec.ts
@@ -0,0 +1,28 @@
+import {inject, TestBed} from '@angular/core/testing';
+import {CategoryService} from './category.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {Category} from '../models/category';
+
+describe('TestService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [CategoryService],
+  }));
+
+  it('should be created', () => {
+    const service: CategoryService = TestBed.get(CategoryService);
+    expect(service).toBeTruthy();
+  });
+  it('should get list categories', inject([CategoryService, HttpTestingController], (service: CategoryService, backend: HttpTestingController) => {
+    const mockCategories: Category[] = [{_id: 'test', name: 'Test'}];
+    expect(service).toBeTruthy();
+    service.getCategoryList().subscribe(categories => {
+      expect(categories).toEqual(mockCategories);
+    });
+
+    backend.expectOne({
+      method: 'GET',
+      url: '/categories'
+    }).flush(mockCategories);
+  }));
+});
